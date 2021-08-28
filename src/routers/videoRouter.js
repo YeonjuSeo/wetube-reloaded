7e6bec7f1f43d2589df3fcdac448d9ff2a352bfb
YeonjuSeo <tjduswn1219@gmail.com>
2021-08-29 02:06:08 +0900

    🎨  라우터 파라미터 추가

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,8 @@ import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload); //'upload'가 id로 인식되지 않도록 가장 위로
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
