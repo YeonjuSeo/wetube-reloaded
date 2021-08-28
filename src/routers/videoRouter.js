38f2952ced811a20d6787d7d75bb55cf3a0040b0
YeonjuSeo <tjduswn1219@gmail.com>
2021-08-29 01:48:01 +0900

    🎨  라우터&컨트롤러 추가

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { watch, edit } from "../controllers/videoController";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", watch);
-videoRouter.get("/edit", edit);
+videoRouter.get("/upload", upload); //'upload'가 id로 인식되지 않도록 가장 위로
+videoRouter.get("/:id", see);
+videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
