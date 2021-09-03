ea226efa0d5c698dc2631a493ddc51e543f5950e
YeonjuSeo <tjduswn1219@gmail.com>
2021-09-04 05:17:04 +0900

    ✨  비디오 이름 바꾸기(POST)

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
+app.use(express.urlencoded({ extended: true })); // express application이 form의 value를 이해할 수 있도록 하고 자바스크립트 형식으로 변환
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
