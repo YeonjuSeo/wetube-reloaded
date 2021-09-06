4a73ff8e3e83f0ad911fbbcdc787cce07c883649
YeonjuSeo <tjduswn1219@gmail.com>
2021-09-06 15:11:38 +0900

    🥅  비디오 생성 에러 핸들링

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,16 +27,18 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  //object 생성 후 DB에 데이터 저장
-  await Video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    //object 생성 후 DB에 데이터 저장
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
