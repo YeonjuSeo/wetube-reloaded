82a5a94132739faee648905fac908f5cc91567ba
YeonjuSeo <tjduswn1219@gmail.com>
2021-09-07 23:54:55 +0900

    ✨  비디오 수정 기능

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,26 +8,47 @@ export const home = async (req, res) => {
     return res.render("server-render");
   }
 };
+
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
-  if (video) {
-    return res.render("watch", { pageTitle: video.title, video });
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
   }
-  return res.render("404", { pageTitle: "Video not found." });
+  return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id); //영상 찾기
+  if (!video) {
+    //비디오가 없으면 404 표시
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
-export const postEdit = (req, res) => {
+
+export const postEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id); //영상 찾기
+  if (!video) {
+    //비디오가 없으면 404 표시
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  video.title = title;
+  video.description = description;
+  video.hashtags = hashtags
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+  await video.save();
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
   try {
