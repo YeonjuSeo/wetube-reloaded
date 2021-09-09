08a3422fa5a65c347f4a3daff6e9e045b1f7719e
YeonjuSeo <tjduswn1219@gmail.com>
2021-09-09 22:40:47 +0900

    🚚  라우터 이름 변경

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import morgan from "morgan";
 
 // Routers
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -13,7 +13,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); // express application이 form의 value를 이해할 수 있도록 하고 자바스크립트 형식으로 변환
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
