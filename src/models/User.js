820ce8ccafbeec0e2fb3070ea0db2d69ac7cb73a
YeonjuSeo <tjduswn1219@gmail.com>
2021-09-09 22:42:00 +0900

    ➕ 🔒️  비밀번호 암호화

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
@@ -6,6 +8,13 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: String,
 });
+
+userSchema.pre("save", async function () {
+  console.log("Users password:", this.password);
+  this.password = await bcrypt.hash(this.password, 5); //여기서의 this = create되는 유저
+  console.log("Hashed password:", this.password);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
